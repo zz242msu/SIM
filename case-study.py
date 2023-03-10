77c8cb1002526d762bec5b255a43e15e5a8d0ab1
zz242msu <121355649+zz242msu@users.noreply.github.com>
2023-03-09 21:20:17 -0500

    add more print comments

--- case-study.py
+++ case-study.py
@@ -9,8 +9,13 @@ import statistics as s
 def analyze_graph(g, config):
 
   print("Graph is on.")
-  #todo info deprecated
-  # print(nx.info(g))
+  print("g: ", g)
+  print("config: ", config)
+
+  print("number of nodes: ", nx.number_of_nodes(g))
+  print("number of edges: ", nx.number_of_edges(g))
+  print("density of the graph: ", nx.density(g))
+  print("whether the graph is directed or not: ", nx.is_directed(g))    
 
   print('------------------------------------------------')
   print('degree')
@@ -20,6 +25,7 @@ def analyze_graph(g, config):
   print('time: ', end - start)
   print('degree: ', set)
 
+  print('Distance among seeds')
   # Distance among seeds
   for i in range(len(set)-1):
     for j in range(i+1, len(set)):
@@ -27,20 +33,20 @@ def analyze_graph(g, config):
 
   # Sobol Total of each seed
   df = simulationIC(100,g,set,config)
-  print(df)
+  print('df', df)
   EY,VY = Y(df)
   print('Expectation: ', EY)
   print('Variance: ', VY)
 
+  print('key : sobols[key]')
   Sobols = sobols(df,set)
   for key in Sobols:
     print(key, ' : ', Sobols[key])
 
+  print('node : ST[node]/VY')
   ST = SobolT(df,set)
   for node in ST.keys():
     print(node, ': ', ST[node]/VY)
-
-
 
   ie, std = IE(df,set)
   print('IE:', ie)
@@ -56,6 +62,7 @@ def analyze_graph(g, config):
   ST2 = []
   ST3 = []
   ST4 = []
+
   for i in range(5):
 
     df = simulationIC(100, g, set, config)
@@ -73,6 +80,7 @@ def analyze_graph(g, config):
     ST3.append(ST[node3] / VY)
     node4 = set[4]
     ST4.append(ST[node4] / VY)
+
     # set[0]
     sim = df[(df[set[0]] == 0) & (df[set[1]] == 1) & (df[set[2]] == 1) & (df[set[3]] == 1) & (df[set[4]] == 1)]
     Esim = s.mean(sim['result']) - ie
@@ -103,24 +111,27 @@ def analyze_graph(g, config):
     MC4.append(Esim)
 
   print('degree: ', set)
+  print('s.mean(MC*) +- s.stdev(MC*)')
   print(s.mean(MC0), ' +- ', s.stdev(MC0))
   print(s.mean(MC1), ' +- ', s.stdev(MC1))
   print(s.mean(MC2), ' +- ', s.stdev(MC2))
   print(s.mean(MC3), ' +- ', s.stdev(MC3))
   print(s.mean(MC4), ' +- ', s.stdev(MC4))
   print("ST-----------------------------")
+  print('s.mean(ST*) +- s.stdev(ST*)')
   print(s.mean(ST0), ' +- ', s.stdev(ST0))
   print(s.mean(ST1), ' +- ', s.stdev(ST1))
   print(s.mean(ST2), ' +- ', s.stdev(ST2))
   print(s.mean(ST3), ' +- ', s.stdev(ST3))
   print(s.mean(ST4), ' +- ', s.stdev(ST4))
 
+  print('rank')
   rank = []
   for node in sorted(ST, key=ST.get, reverse = True):
     rank.append(node)
   print(rank)
   
-
+# config the graphs
 print("Analyzing ", 'Cora')
 g, config = Cora()
 analyze_graph(g, config)
